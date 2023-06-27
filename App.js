a9fd896cabe9e9fa7e405759916369cf0f5c6ba0
myaooooo98 <yaoer0505@gmail.com>
2023-06-27 23:55:19 +0800

    learn basic of react native

--- App.js
+++ App.js
@@ -1,11 +1,36 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import react from 'react';
+import { StyleSheet, Text, TouchableOpacity, View } from 'react-native';
 
 export default function App() {
+  // const [ number1, setNumber1 ] = react.useState(0)
+
+  // const addNumber = () => {
+  //   let newNumber = number1 + 1
+  //   setNumber1(newNumber)
+  // }
+
+  // const subtractNumber = () => {
+  //   let newNumber = number1 - 1
+  //   setNumber1(newNumber)
+  // } 
+
+  const [ notification , setNotification ] = react.useState('Player X to start')
+
+  const [ board, setBoard ] = react.useState(
+    [
+      " ", " ", " ",
+      " ", " ", " ",
+      " ", " ", " "
+    ]
+  )
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <StatusBar style="auto" /> 
+
+      <Text style={styles.txt1}>Tic Tac Toe</Text>
+      <Text style={styles.txt2}>{notification}</Text> 
     </View>
   );
 }
@@ -17,4 +42,10 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+  txt1: {
+    fontSize: 50,
+  },
+  txt2: {
+    fontSize: 20,
+  },  
 });
